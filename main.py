e34a361ad8a0aab4f6b3e15f8e58ecde3f7847ff
Hayden Ferguson <haydenf1204@gmail.com>
2025-11-17 15:26:17 -0500

    Created fill_database()

--- main.py
+++ main.py
@@ -18,5 +18,17 @@ def create_tables():
     except (psycopg2.DatabaseError, Exception) as error:
         print(error)
 
+def fill_database(value_list):
+    sql = "INSERT INTO placeholder(value) VALUES(%s) RETURNING *"
+    config = load_config()
+    try:
+        with  psycopg2.connect(**config) as conn:
+            with  conn.cursor() as cur:
+                cur.executemany(sql, value_list)
+
+            conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
 if __name__ == '__main__':
     create_tables()
