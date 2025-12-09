0cead7278ed3c48e9d490010a136b8c13c0bcfed
Hayden Ferguson <haydenf1204@gmail.com>
2025-12-09 13:17:05 -0500

    Added test_read_json()

--- tests/test_json_reader.py
+++ tests/test_json_reader.py
@@ -1,4 +1,5 @@
 import src.json_reader as json_reader
+import json
 import os
 import pytest # pyright: ignore[reportMissingImports]
 
@@ -7,3 +8,12 @@ def test_convert_json():
     results = json_reader.convert_json(dictonaries, ["a", "years_with_current_manager"])
     assert results[0] == ["b", 3]
     assert results[1] == ["C", 4]
+
+def test_read_json():
+    dictonaries = [{"A":"b", "Years_With_Curr_Manager":3}, {"yearswithcurrentmanager":4, "a":"C"}]
+    with open("test.json", "w") as file:
+        json.dump(dictonaries, file, indent=4)
+    results = json_reader.read_json("test.json", ["a", "years_with_current_manager"])
+    assert results[0] == ["b", 3]
+    assert results[1] == ["C", 4]
+    os.remove("test.json")
